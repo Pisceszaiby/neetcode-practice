5051af39c33a5df2ac817697a13d2b74013c37a8
Zainab Anwaar <52016745+Pisceszaiby@users.noreply.github.com>
2026-06-29 02:56:02 +0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    // isAnagram(s, t) {
+    //     if (s.length !== t.length) {
+    //         return false
+    //     }
+    //     const arrayS = s.split('')?.sort();
+    //     const arrayT = t.split('')?.sort();
+
+    //     for (let i in arrayS) {
+    //         if (arrayS[i] != arrayT[i])
+    //             return false
+    //     }
+    //     return true;
+
+    // }
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false
+        }
+        const hashS = {}
+
+        for (let i in s) {
+            hashS[s[i]] = hashS[s[i]] ? hashS[s[i]] + 1 : 1;
+        }
+        for (let j in t) {
+            if (hashS[t[j]] === undefined) return false;
+            hashS[t[j]] = hashS[t[j]] - 1;
+        }
+
+        for (let i of Object.values(hashS)){
+            if (i !==0){
+                return false
+            }
+        }
+        return true
+
+
+    }
+}
