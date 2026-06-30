7b3dc3aaa65de91927866ddebb7d0143dfa4145b
Zainab Anwaar <52016745+Pisceszaiby@users.noreply.github.com>
2026-06-30 22:21:56 +0500

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,18 @@
+class Solution {
+    twoSum(numbers, target) {
+        let l = 0;
+        let r = numbers.length - 1;
+
+        while (l < r) {
+            const sum = numbers[l] + numbers[r];
+
+            if (sum === target) {
+                return [l + 1, r + 1];
+            } else if (sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+    }
+}
